2be1a1d4b1da515b74a9fa320bd21b92f0a27d29
이정국 <a6376705@gmail.com>
2026-03-16 21:32:16 +0900

    기능 : TextRenderer 구현

---
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/TextRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class TextRenderer : Renderer
+{  
+    private ConsolePoint mScrenPos;
+    private string mText;
+
+    public TextRenderer(int screnPosX, int screnPosY, string text)
+    {
+        mScrenPos.x = screnPosX;
+        mScrenPos.y = screnPosY;
+        mText = text;
+    }
+
+    override public void Draw()
+    {
+        Console.SetCursorPosition(mScrenPos.x, mScrenPos.y);
+        Console.Write(mText);
+    }
+}
+
