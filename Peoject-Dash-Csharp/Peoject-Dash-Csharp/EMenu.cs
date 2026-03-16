7c048800d37c1b8607b4d89b52d29254dfcba3a9
이정국 <a6376705@gmail.com>
2026-03-16 21:50:47 +0900

    기능 : EMenu, MenuScene 구현

---
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/EMenu.cs
@@ -0,0 +1,5 @@
+﻿enum EMenu
+{
+    None = 0,
+    Max
+}
---
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/MenuScene.cs
@@ -0,0 +1,20 @@
+﻿class MenuScene : Scene
+{
+    private UI[] mMenuUIs;
+
+    public MenuScene()
+    {
+        InitMenus();
+    }
+    public void InitMenus() 
+    {
+        mMenuUIs = new UI[(int)EMenu.Max];
+    }
+    public void CreateMenu(EMenu UIName, int UIPosX, int UIPosY)
+    {
+        UI mMenuUI = new UI();
+        mMenuUIs[(int)UIName] = mMenuUI;
+        
+        mMenuUI.SetScreenPos(ScreenPosX: UIPosX, ScreenPosY: UIPosY);        
+    }
+}
