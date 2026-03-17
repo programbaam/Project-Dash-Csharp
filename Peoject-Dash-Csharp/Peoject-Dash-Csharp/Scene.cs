d41c1f23000dc9f6d3f888df8506d414f15d6e75
이정국 <a6376705@gmail.com>
2026-03-17 17:27:19 +0900

    기능 : Scene클래스의 Release 함수 구현 내부코드 함수화

--- Peoject-Dash-Csharp/Peoject-Dash-Csharp/Scene.cs
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/Scene.cs
@@ -9,23 +9,54 @@ abstract class Scene
     private readonly HashSet<GameObject> mNewGameObjects = new();
     private readonly HashSet<GameObject> mDeleteGameObjects = new();
 
+    protected void InitGameObject(GameObject gameObject)
+    {
+        mGameObjects.Add(gameObject);
+
+        if (gameObject is IInputable inputable)
+        {
+            Debug.Assert(inputable != null);
+            mSyncSet.newInputs.Add(inputable);
+        }
+    }
+    
     virtual public void Init()
     {
         foreach (GameObject gameObject in mNewGameObjects)
         {          
-
-            if (gameObject is IInputable inputable)
-            {
-                Debug.Assert(inputable != null);
-                mSyncSet.newInputs.Add(inputable);
-            }
+           InitGameObject(gameObject);
         }
     }   
     virtual public void Update()
-    { }
-    virtual public void Delete()
-    { }
+    {
+        foreach (GameObject gameObject in mGameObjects)
+        {
+           gameObject.Update();
+        }
+    }
+    protected void ReleaseGameObject(GameObject gameObject) 
+    {
+        mDeleteGameObjects.Add(gameObject);
+        if (gameObject is IInputable inputable)
+        {
+            Debug.Assert(inputable != null);
+            mSyncSet.deleteInputs.Add(inputable);
+        }
+    }
+    virtual public void Release()
+    {
+        
+        foreach (GameObject gameObject in mGameObjects)
+        {
+            ReleaseGameObject(gameObject);
+        }
+    }
     virtual public void Destroy()
-    { }
+    {
+        foreach (GameObject gameObject in mDeleteGameObjects)
+        {
+            gameObject.Destroy();
+        }
+    }
 
 }
