da8124e158059c418da226cd8deea031e66277f5
이정국 <a6376705@gmail.com>
2026-03-17 16:43:03 +0900

    기능 : GameObject에 해시셋, Init, Update, Delete, Destroy 추가

--- Peoject-Dash-Csharp/Peoject-Dash-Csharp/GameObject.cs
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/GameObject.cs
@@ -1,4 +1,37 @@
-﻿abstract class GameObject
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+abstract class GameObject
 {
     private readonly SyncSet mSyncSet;
+
+    private readonly HashSet<Component> mComponents= new();
+    private readonly HashSet<Component> mNewComponents= new();
+    private readonly HashSet<Component> mDeleteComponents= new();
+    
+
+    virtual public void Init()
+    {
+        foreach (var component in mNewComponents)
+        {
+            
+            if (component is Renderer renderer)
+            {
+                Debug.Assert(renderer != null);
+                mSyncSet.newRenderers.Add(renderer);
+            }
+            if (component is IInputable inputable)
+            {
+                Debug.Assert(inputable != null);
+                mSyncSet.newInputs.Add(inputable);                
+            }
+        }
+    }
+    virtual public void Update()
+    { }
+    virtual public void Delete() 
+    { }
+    abstract public void Destroy();
+    
+        
 }
