078e24c51006616c6ea7d0556bd200c1bf8d7ca1
이정국 <a6376705@gmail.com>
2026-03-17 17:36:02 +0900

    기능 : GameObject클래스의 release함수 구현과 함수 내부화

--- Peoject-Dash-Csharp/Peoject-Dash-Csharp/GameObject.cs
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/GameObject.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 
 abstract class GameObject
@@ -8,30 +9,57 @@ abstract class GameObject
     private readonly HashSet<Component> mComponents= new();
     private readonly HashSet<Component> mNewComponents= new();
     private readonly HashSet<Component> mDeleteComponents= new();
-    
+
+    protected void InitComponent(Component component) 
+    {
+        mComponents.Add(component);
+
+        if (component is Renderer renderer)
+        {
+            Debug.Assert(renderer != null);
+            mSyncSet.newRenderers.Add(renderer);
+        }
+        if (component is IInputable inputable)
+        {
+            Debug.Assert(inputable != null);
+            mSyncSet.newInputs.Add(inputable);
+        }
+    }
+    protected void ReleaseComponent(Component component)
+    {
+        mComponents.Add(component);
+
+        if (component is Renderer renderer)
+        {
+            Debug.Assert(renderer != null);
+            mSyncSet.deleteRenderers.Add(renderer);
+        }
+        if (component is IInputable inputable)
+        {
+            Debug.Assert(inputable != null);
+            mSyncSet.deleteInputs.Add(inputable);
+        }
+    }
 
     virtual public void Init()
     {
+        Debug.Assert(mDeleteComponents.Count == 0);
         foreach (Component component in mNewComponents)
         {
-            mComponents.Add(component);
-            
-            if (component is Renderer renderer)
-            {
-                Debug.Assert(renderer != null);
-                mSyncSet.newRenderers.Add(renderer);
-            }
-            if (component is IInputable inputable)
-            {
-                Debug.Assert(inputable != null);
-                mSyncSet.newInputs.Add(inputable);                
-            }
+            InitComponent(component);
         }
+        mNewComponents.Clear();
     }
     virtual public void Update()
     { }
-    virtual public void Delete() 
-    { }
+    virtual public void Release() 
+    {
+        foreach (Component component in mComponents)
+        {
+            ReleaseComponent(component);
+        }
+        mComponents.Clear();
+    }
     virtual public void Destroy()
     { }
     
