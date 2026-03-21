336ee00d521a6bbdfd3570e9820e5e4d627efd7f
programbaam <programbaam@naver.com>
2026-03-21 20:33:26 +0900

    기능 : Actor 기능 추가

---
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/Actor.cs
@@ -0,0 +1,28 @@
+﻿class Actor : GameObject
+{
+    private Vector2D mWorldLocation;
+    private Text2DRenderer mText2D;
+
+    public bool IsVisible
+    {
+        get => mText2D.IsDrawing;
+        set => mText2D.IsDrawing = value;
+    }
+    public override void Update()
+    {
+        
+    }
+
+    Actor(Vector2D worldLocation, string[] text2D)
+    {
+        this.mWorldLocation = worldLocation;
+
+        ConsolePoint screenPos;
+
+        screenPos.x = (int)worldLocation.x;
+        screenPos.y = (int)worldLocation.y;
+
+        this.mText2D = new Text2DRenderer(screenPos, text2D);
+    }
+}
+
