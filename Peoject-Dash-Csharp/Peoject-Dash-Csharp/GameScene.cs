3258192f2747fdbd889e216484bd0787361c30c9
JiwonLee <2jiwons2@gmail.com>
2026-03-22 02:56:27 +0900

    기능: 게임씬 UI 구현

--- Peoject-Dash-Csharp/Peoject-Dash-Csharp/GameScene.cs
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/GameScene.cs
@@ -3,6 +3,7 @@ using System;
 
 class GameScene : Scene
 {
+    private readonly Widget mScoreWidget;
     private readonly Player mPlayer;
     private readonly Actor mObstacle;
     private readonly Actor mGround;
@@ -13,6 +14,29 @@ class GameScene : Scene
 
     public GameScene()
     {
+        ConsolePoint consolePoint;
+        Vector2D worldLocation;
+
+        //Widget 사용시 LOCAL_CONTENTS_POS_X 값만큼 빼줘야 함
+        consolePoint.x = 74 - 4;
+        consolePoint.y = 1;
+
+        NewGameObject(new Widget(consolePoint, "게임 점수 :", false));
+
+        consolePoint.x = 86 - 4;
+        consolePoint.y = 1;
+
+        //TODO 실시간 변화하는 위젯 만들어야 함
+        mScoreWidget = new Widget(consolePoint, $"{Time.DeltaTime,13:N0}", false);
+        NewGameObject(mScoreWidget);
+
+        consolePoint.x = 0 - 4;
+        consolePoint.y = 3;
+
+        string Divider = "".PadRight(GameManager.ConsoleWidth, '-'); //Console.Width
+
+        NewGameObject(new Widget(consolePoint, Divider, false));
+
         string[] playerTexts =
         {
             " * ",
@@ -20,9 +44,8 @@ class GameScene : Scene
             "* *"
         };
 
-        Vector2D worldLocation;
         worldLocation.x = 5.0f;
-        worldLocation.y = 12.0f;
+        worldLocation.y = 13.0f;
 
         mPlayer = new Player(worldLocation: worldLocation,text2D: playerTexts, jumpPower: 10, color: ConsoleColor.Magenta);
         NewGameObject(mPlayer);
@@ -56,6 +79,16 @@ class GameScene : Scene
 
         mGround = new Actor(worldLocation: worldLocation, text2D: groundTexts, color: ConsoleColor.Green);
         NewGameObject(mGround);
+
+        consolePoint.x = 0 - 4;
+        consolePoint.y = 20;
+
+        NewGameObject(new Widget(consolePoint, Divider, false));
+
+        consolePoint.x = 0 - 4;
+        consolePoint.y = 21;
+
+        NewGameObject(new Widget(consolePoint, "게임종료 : ESC 키", false));
     }
 }
 
