da9e0b84697cf7837f1332ac7afa1118efa3fcd4
programbaam <programbaam@naver.com>
2026-03-21 20:33:11 +0900

    기능 : Text2DRenderer 클래스 추가

---
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/Text2DRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+class Text2DRenderer : Renderer
+{
+    private ConsolePoint mScreenPos;
+    private string[] mText2D;
+
+    public override void Draw()
+    {
+        if (IsDrawing == false)
+        {
+            return;
+        }
+
+        Debug.Assert(mText2D != null);
+        for (int i = 0; i < mText2D.Length; i++)
+        {
+            Console.SetCursorPosition(mScreenPos.x, mScreenPos.y + i);
+            Console.Write(mText2D[i]);
+        }
+    }
+
+    public Text2DRenderer(ConsolePoint screenPos, string[] text2D, bool isDrawing = true)
+    {
+        mScreenPos = screenPos;
+        IsDrawing = isDrawing;
+
+        mText2D = text2D;
+    }
+}
+
