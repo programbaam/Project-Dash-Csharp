ef220cdc950b75b173ec0cb1aa17fdff468e9938
programbaam <programbaam@naver.com>
2026-03-21 22:39:03 +0900

    기능 : GameScene 클래스 추가

---
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/GameScene.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+
+class GameScene : Scene
+{
+    private readonly Actor mPlayer;
+    private readonly Actor mObstacle;
+    private readonly Actor mGround;
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public GameScene()
+    {
+        string[] playerTexts =
+        {
+            " * ",
+            "***",
+            "* *"
+        };
+
+        Vector2D worldLocation;
+        worldLocation.x = 5.0f;
+        worldLocation.y = 12.0f;
+
+        mPlayer = new Actor(worldLocation: worldLocation,text2D: playerTexts, color: ConsoleColor.Magenta);
+        NewGameObject(mPlayer);
+
+        string[] obstacleTexts =
+        {
+            "****",
+            " ** ",
+            " ** ",
+            " ** ",
+        };
+
+        worldLocation.x = 50.0f;
+        worldLocation.y = 12.0f;
+        mObstacle = new Actor(worldLocation: worldLocation, text2D: obstacleTexts, color: ConsoleColor.Blue);
+        NewGameObject(mObstacle);
+
+
+        worldLocation.x = 0.0f;
+        worldLocation.y = 16.0f;
+
+        string groundPad = "".PadRight(GameManager.ConsoleWidth, '*'); //Console.Width
+
+        string[] groundTexts =
+        {
+            groundPad,
+            groundPad,
+            groundPad,
+            groundPad,
+        };
+
+        mGround = new Actor(worldLocation: worldLocation, text2D: groundTexts, color: ConsoleColor.Green);
+        NewGameObject(mGround);
+    }
+}
+
