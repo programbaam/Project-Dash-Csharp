1eec1c5cde7d8a7571e5e1dbd8adef129a29d2a9
programbaam <programbaam@naver.com>
2026-03-21 22:38:11 +0900

    수정 : GameMath 클래스 엡실론 구현

--- Peoject-Dash-Csharp/Peoject-Dash-Csharp/GameMath.cs
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/GameMath.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Peoject_Dash_Csharp
+static class GameMath
 {
-    internal class GameMath
+    public const float EPSILON = 0.00001f;
+    // 콘솔 화면 임계값
+    public const float VIEW_THRESHOLD = 0.5f;
+
+    public static bool IsNearlyEqual(float lvalue, float rvalue)
     {
+        return Math.Abs(lvalue - rvalue) < EPSILON;
+    }
+
+    public static bool IsZero(float value)
+    {
+        return Math.Abs(value) < EPSILON;
+    }
+
+    public static int ToScreenCoord(float value)
+    {
+        int roundDownValue = (int)value;
+        float downDecimalPointvalue = value - roundDownValue;
+
+        if (Math.Abs(downDecimalPointvalue) < VIEW_THRESHOLD)
+        {
+            return roundDownValue;
+        }
+
+        return value > 0 ? roundDownValue + 1 : roundDownValue - 1;
     }
 }
+
