3dcdbdaf7a7fe403d22ac411cbc3817e3dc3a7c4
programbaam <programbaam@naver.com>
2026-03-22 15:40:36 +0900

    추가 : CollisionManager 클래스 추가

---
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/CollisionManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.JavaScript;
+
+class CollisionManager
+{
+    private List<(Collider collider, ICollidable owner)> mCollisionList = new();
+
+    private static bool[,] sLayerTable =
+    {
+        //              Player,     Obstacle
+        /*Player*/      { false,    true },
+        /*Obstacle*/    { true,     false },
+    };
+
+    public static bool GetLayerTable(ELayerTag collier1, ELayerTag collier2)
+    {
+        return sLayerTable[(int)collier1, (int)collier2];
+    }
+
+    public void UnionWithNewCollision()
+    {
+        foreach((Collider collider, ICollidable owner) collision in GameManager.mSyncSet.newCollision)
+        {
+            if (mCollisionList.Contains(collision))
+            {
+                continue;
+            }
+
+            mCollisionList.Add(collision);
+        }
+    }
+
+    public void ExceptWithDeleteCollision()
+    {
+        foreach ((Collider collider, ICollidable owner) collision in GameManager.mSyncSet.newCollision)
+        {
+            if (mCollisionList.Contains(collision))
+            {
+                mCollisionList.Remove(collision);
+            }
+        }
+    }
+
+    public void Collision()
+    {
+        
+        for (int i = 0; i < mCollisionList.Count; i++)
+        {
+
+            for (int j = i + 1; j < mCollisionList.Count; j++)
+            {
+                if (mCollisionList[i].collider.IsOverlap(
+                        mCollisionList[j].collider))
+                {
+                    mCollisionList[i].owner.OnCollision();
+                }
+            }
+        }
+    }
+}
+
