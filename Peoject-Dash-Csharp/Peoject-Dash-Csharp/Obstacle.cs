01995a3452f8a080db6e30c183ee3c971e11c08c
programbaam <programbaam@naver.com>
2026-03-22 17:29:10 +0900

    수정 : Obstacle 클래스 ICollidable 및 Collider 추가

--- Peoject-Dash-Csharp/Peoject-Dash-Csharp/Obstacle.cs
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/Obstacle.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Drawing;
 
-class Obstacle: Actor
+class Obstacle: Actor, ICollidable
 {
+    private Collider mCollider;
+
     const float OBSTACLE_SPEED = 4.0f;
 
     private float mObstacleSpeed;   
@@ -19,14 +21,30 @@ class Obstacle: Actor
         Vector2D curruntPos = WorldLocation;
 
         curruntPos.x -= mObstacleSpeed * Time.DeltaTime;
+
         WorldLocation = curruntPos;
+        mCollider.WorldLocation = curruntPos;
     }
 
-    
+    public void OnCollision()
+    {
+
+    }
+
+
 
     public Obstacle(Vector2D worldLocation, string[] text2D, ConsoleColor color = ConsoleColor.White) : base(worldLocation, text2D, color)
     {
         mObstacleSpeed = OBSTACLE_SPEED;
+
+        Rect rect;
+        rect.left = 0.0f;
+        rect.right = text2D[0].Length;
+        rect.top = 0.0f;
+        rect.bottom = text2D.Length;
+
+        mCollider = new Collider(worldLocation, rect, ELayerTag.Obstacle);
+        NewComponemt(mCollider);
     }
 
 
