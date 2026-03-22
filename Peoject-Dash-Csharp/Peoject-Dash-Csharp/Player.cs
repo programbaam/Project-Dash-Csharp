3eb6762ed4ca23c514f2818dbdb4cc211d890b00
programbaam <programbaam@naver.com>
2026-03-22 17:28:35 +0900

    수정 : Player 클래스 Collider, ICollidable 추가 및 Esc 입력 처리 추가

--- Peoject-Dash-Csharp/Peoject-Dash-Csharp/Player.cs
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/Player.cs
@@ -1,18 +1,30 @@
 ﻿
 using System;
 
-class Player : Actor, IInputable
+class Player : Actor, IInputable, ICollidable
 {
     private RigidBody2D mRigidBody2D;
     private float mJumpPower;
     private bool mIsJumping;
+    private Collider mCollider;
     public void Input()
     {
+        if (InputManager.IsCurrentKeyDown(EVirtualKey.ESC))
+        {
+            GameManager.mSyncSet.scene = EScene.Menu;
+            GameManager.mSyncSet.isChangeScene = true;
+        }
+
         if (mIsJumping == false && InputManager.IsCurrentKeyDown(EVirtualKey.SPACE))
         {
             mIsJumping = true;
             mRigidBody2D.AddVelocityY(mJumpPower);
         }
+    }
+
+    public void OnCollision()
+    {
+        
     }
 
     public override void Update()
@@ -25,6 +37,7 @@ class Player : Actor, IInputable
                 mRigidBody2D.UpdatePositionY(ref currentLocation);
 
                 WorldLocation = currentLocation;
+                mCollider.WorldLocation = currentLocation;
             }
 
             if (mRigidBody2D.IsPhysicsActive == false)
@@ -38,7 +51,17 @@ class Player : Actor, IInputable
     {
         mJumpPower = jumpPower;
         mIsJumping = false;
-        mRigidBody2D = new RigidBody2D(gravity: 0.0625f, topY: 5.0f, groundY: 13.0f);
+        mRigidBody2D = new RigidBody2D(gravity: 1f, topY: 5.0f, groundY: 13.0f);
+        NewComponemt(mRigidBody2D);
+
+        Rect rect;
+        rect.left = 0.0f;
+        rect.right = text2D[0].Length;
+        rect.top = 0.0f;
+        rect.bottom = text2D.Length;
+
+        mCollider = new Collider(worldLocation, rect, ELayerTag.Player);
+        NewComponemt(mCollider);
     }
 
 }
