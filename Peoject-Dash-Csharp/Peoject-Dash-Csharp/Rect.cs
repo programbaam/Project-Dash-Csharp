32d46190a9eecf7fb44a576dc889c0ccc4c0e1a7
programbaam <programbaam@naver.com>
2026-03-22 15:07:11 +0900

    수정 : Rect 구조체 생성자 추가

--- Peoject-Dash-Csharp/Peoject-Dash-Csharp/Rect.cs
+++ Peoject-Dash-Csharp/Peoject-Dash-Csharp/Rect.cs
@@ -4,5 +4,13 @@
     public float right;
     public float top;
     public float bottom;
+
+    public Rect(float left, float right, float top, float bottom)
+    {
+        this.left = left;
+        this.right = right;
+        this.top = top;
+        this.bottom = bottom;
+    }
 }
 
